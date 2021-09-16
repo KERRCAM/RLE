5c20b75e30bf3a257847b3517d28e4ed581283c4
KERRCAM <kerrcameron@hotmail.com>
2021-09-16 22:36:54 +0100

    while loop start

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
+        int indPos = 0;
         Scanner textToTranslate = new Scanner(System.in);
         System.out.println("enter some text: ");
         String text = textToTranslate.next();
@@ -13,7 +13,12 @@ public class Main {
         int loopRunsLeft = textLength;
         while(loopRunsLeft>0) {
             loopRunsLeft=(loopRunsLeft-1);
-            
+            char character = text.charAt(indPos);
+            System.out.println(character);
+
+            indPos = (indPos+1);
+
+
             }
 
 
