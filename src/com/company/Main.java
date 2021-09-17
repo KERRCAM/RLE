b24ad1ec75026cc88a5247fecca7ffe77dacafd1
KERRCAM <kerrcameron@hotmail.com>
2021-09-17 23:36:38 +0100

    RLE compression V4 Almost done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,8 @@
 package com.company;
 import java.util.Scanner;
+
+/*
+//while loop version, cur vs prev, index in string
 public class Main {
 
     public static void main(String[] args) {
@@ -13,6 +16,7 @@ public class Main {
         int textLength = text.length();
         System.out.println(textLength);
         int loopRunsLeft = textLength;
+        //start while loop
         while(loopRunsLeft>0) {
             loopRunsLeft=(loopRunsLeft-1);
             char character = text.charAt(indPos);
@@ -26,9 +30,12 @@ public class Main {
                     consecChar = 1;
                 }
             }
-            //rle print code to go here.
+            System.out.print(currChar);
+            System.out.print(consecChar);
+            consecChar = 1;
             prevChar = character;
             indPos = (indPos+1);
+        //end while loop
         }
 
 
@@ -37,5 +44,57 @@ public class Main {
 
 
 }
+*/
+//for loop version, cur vs next, index in array (had help for 2nd try)
+public class Main {
+    public static void main(String[] args) {
+        int consecChar = 1;
+        int consecCharLengthTally = 0;
+        Scanner textToTranslate = new Scanner(System.in);
+        System.out.println("enter some text: ");
+        String text = textToTranslate.next();
+        int textLength = text.length();
+        textLength = textLength-1; //adjusts for corect index positions
+        System.out.println(textLength);
+        char[] textToCompress = new char[text.length()]; //initialises list
+        //start for loop
+        for (int i = 0; i<textLength; i++){    // int made 0, runs loop if i is less then length of text and adds 1 to i per loop(what i++ does)
+            textToCompress[i] = text.charAt(i);  // puts each char in list individually ie aaabbb becomes ['a','a','a','b','b','b']
+        }
+        //end for loop
+        char currChar = textToCompress[0];
+        //start for loop
+        for (int i = 0; i<textLength; i++) {
+            int currIndex = i;
+            int nextIndex = i+1;
+            currChar = textToCompress[currIndex];
+            char nextChar = textToCompress[nextIndex];
+            if (currChar == nextChar) {
+                consecChar = consecChar + 1;
+            }
+            else {
+                System.out.print(currChar);
+                System.out.print(consecChar);
+                int consecCharLength = String.valueOf(consecChar).length();
+                consecCharLengthTally = (consecCharLengthTally + consecChar);
+                consecChar = 1;
+                }
+            }
+        //end for loop
+        int finalTextLength = (consecCharLengthTally+1);
+        //check for characters saved not saved or gained
+        System.out.println(":");
+        if (finalTextLength < textLength) {
+            int charactersSaved = (textLength-finalTextLength);
+            System.out.println("well done you saved " + (charactersSaved) + " characters");
+        }
+        if (finalTextLength == textLength) {
+            System.out.println("you saved nothing, absolutely nothing....");
+        }
+        if (finalTextLength > textLength) {
+            int charactersGained = (finalTextLength - textLength);
+            System.out.println("RLW was useless you gained " + (charactersGained) + " characters!!! unless you wanted that...");
+        }
 
-
+    }
+}
